a6e21fff3e9f10a760b87f5a6db10a1431443206
pradeep4382 <41694249+pradeep4382@users.noreply.github.com>
2026-08-09 19:57:01 +0300

    add test

---
+++ UnitTestProject1/StudentServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StudentServiceTests
+    {
+        [TestMethod]
+        public void GetAll_ShouldReturnSeededStudents()
+        {
+            var service = new StudentService();
+            // Act
+            var students = service.GetAll();
+            // Assert
+
+
+            Assert.IsNotNull(students);
+            Assert.AreEqual(2, students.Count);
+            Assert.AreEqual("John", students[0].Name);
+            Assert.AreEqual("David", students[1].Name);
+        }
+    }
+}
---
+++ WebApplication1/StudentService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class StudentService
+    {
+        private readonly List<Student> _students;
+
+        public StudentService()
+        {
+       _students = new List<Student>
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "John",
+                Email = "john@example.com"
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "David",
+                Email = "david@example.com"
+            }
+        };
+        }
+
+        public List<Student> GetAll()
+        {
+            return _students;
+        }
+    }
+}
