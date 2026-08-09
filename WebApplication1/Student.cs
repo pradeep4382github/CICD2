bfce92da842255b474e09f6304d24d1b9807bbdc
pradeep4382 <41694249+pradeep4382@users.noreply.github.com>
2026-08-09 20:37:07 +0300

    hi

--- WebApplication1/Student.cs
+++ WebApplication1/Student.cs
@@ -7,6 +7,8 @@ namespace WebApplication1
 {
     public class Student
     {
+
+
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
